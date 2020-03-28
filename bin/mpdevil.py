6723f9b31b30986d8151697fde6b4c5c8df1b93c
Martin Wagner <grisu-04@web.de>
2020-03-28 13:35:35 +0100

    fixed 'SeekBar' when paused

--- bin/mpdevil.py
+++ bin/mpdevil.py
@@ -2248,6 +2248,8 @@ class SeekBar(Gtk.Box):
 				self.jumped=False
 			self.scale.set_has_origin(True)
 			self.update=True
+			if self.timeout_id == None:
+				self.refresh()
 
 	def on_change_value(self, range, scroll, value): #value is inaccurate
 		if scroll == Gtk.ScrollType.STEP_BACKWARD:
