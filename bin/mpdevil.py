51489bfcae9313732c0ff9a04323931b1f11eada
Martin Wagner <grisu-04@web.de>
2020-07-03 23:11:33 +0200

    updated email

--- bin/mpdevil.py
+++ bin/mpdevil.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # mpdevil - MPD Client.
-# Copyright 2020 Martin Wagner
+# Copyright 2020 Martin Wagner <martin.wagner.dev@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
