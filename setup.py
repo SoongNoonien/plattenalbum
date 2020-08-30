42eec870d85a711a1c20a373c93e74ea594245e8
Martin Wagner <martin.wagner.dev@gmail.com>
2020-08-30 18:04:26 +0200

    install additional icon sizes

--- setup.py
+++ setup.py
@@ -12,8 +12,15 @@ DistUtilsExtra.auto.setup(
 	url="https://github.com/SoongNoonien/mpdevil",
 	license='GPL-3.0',
 	data_files=[
-		('share/icons/hicolor/48x48/apps/', ['data/mpdevil.png']),
-		('share/icons/hicolor/scalable/apps/', ['data/mpdevil.svg'])
+		('share/icons/hicolor/16x16/apps/', ['data/icons/16x16/mpdevil.png']),
+		('share/icons/hicolor/22x22/apps/', ['data/icons/22x22/mpdevil.png']),
+		('share/icons/hicolor/24x24/apps/', ['data/icons/24x24/mpdevil.png']),
+		('share/icons/hicolor/32x32/apps/', ['data/icons/32x32/mpdevil.png']),
+		('share/icons/hicolor/48x48/apps/', ['data/icons/48x48/mpdevil.png']),
+		('share/icons/hicolor/64x64/apps/', ['data/icons/64x64/mpdevil.png']),
+		('share/icons/hicolor/128x128/apps/', ['data/icons/128x128/mpdevil.png']),
+		('share/icons/hicolor/256x256/apps/', ['data/icons/256x256/mpdevil.png']),
+		('share/icons/hicolor/scalable/apps/', ['data/icons/scalable/mpdevil.svg'])
 	],
 )
 
