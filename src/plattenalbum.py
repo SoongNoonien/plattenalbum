df2d0a5fda9f2f77acd585d1d03cf6c449108fce
Martin Wagner <martin.wagner.dev@gmail.com>
2024-03-13 14:19:09 +0100

    removed dead code

--- src/plattenalbum.py
+++ src/plattenalbum.py
@@ -62,26 +62,6 @@ def lookup_icon(icon_name, size, scale=1):
 	return Gtk.IconTheme.get_for_display(Gdk.Display.get_default()).lookup_icon(
 			icon_name, None, size, scale, Gtk.TextDirection.NONE, Gtk.IconLookupFlags.FORCE_REGULAR)
 
-def main_thread_function(func):
-	@functools.wraps(func)
-	def wrapper_decorator(*args, **kwargs):
-		def glib_callback(event, result, *args, **kwargs):
-			try:
-				result.append(func(*args, **kwargs))
-			except Exception as e:  # handle exceptions to avoid deadlocks
-				result.append(e)
-			event.set()
-			return False
-		event=threading.Event()
-		result=[]
-		idle_add(glib_callback, event, result, *args, **kwargs)
-		event.wait()
-		if isinstance(result[0], Exception):
-			raise result[0]
-		else:
-			return result[0]
-	return wrapper_decorator
-
 #########
 # MPRIS #
 #########
