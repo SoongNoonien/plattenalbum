ce18817ea687d5ce49701c890c532be77f5c76b1
Martin Wagner <martin.wagner.dev@gmail.com>
2026-08-07 21:51:09 +0200

    Merge `ListModel` into `SelectionModel`

--- src/plattenalbum.py
+++ src/plattenalbum.py
@@ -1084,35 +1084,19 @@ class HeadingBox(Gtk.Box):
 		self.append(Gtk.Label(label=heading, xalign=0, css_classes=["heading"]))
 		self.append(widget)
 
-class ListModel(GObject.Object, Gio.ListModel):
-	def __init__(self, item_type):
-		super().__init__()
-		self.data=[]
-		self._item_type=item_type
-
-	def do_get_item(self, position):
-		try:
-			return self.data[position]
-		except IndexError:
-			return None
-
-	def do_get_item_type(self):
-		return self._item_type
-
-	def do_get_n_items(self):
-		return len(self.data)
-
-class SelectionModel(ListModel, Gtk.SelectionModel):
+class SelectionModel(GObject.Object, Gio.ListModel, Gtk.SelectionModel):
 	__gsignals__={"selected": (GObject.SignalFlags.RUN_FIRST, None, (int,)),
 			"reselected": (GObject.SignalFlags.RUN_FIRST, None, ()),
 			"clear": (GObject.SignalFlags.RUN_FIRST, None, ())}
 	def __init__(self, item_type):
-		super().__init__(item_type)
+		super().__init__()
+		self._item_type=item_type
+		self._data=[]
 		self._selected=None
 
 	def clear(self, position=0):
 		n=self.get_n_items()-position
-		self.data=self.data[:position]
+		self._data=self._data[:position]
 		if self._selected is not None and self._selected >= self.get_n_items():
 			self._selected=None
 		self.items_changed(position, n, 0)
@@ -1121,18 +1105,18 @@ class SelectionModel(ListModel, Gtk.SelectionModel):
 
 	def append(self, data):
 		n=self.get_n_items()
-		self.data.extend(data)
+		self._data.extend(data)
 		self.items_changed(n, 0, self.get_n_items())
 
 	def get_selected(self):
 		return self._selected
 
 	def set(self, position, item):
-		if position < len(self.data):
-			self.data[position]=item
+		if position < len(self._data):
+			self._data[position]=item
 			self.items_changed(position, 1, 1)
 		else:
-			self.data.append(item)
+			self._data.append(item)
 			self.items_changed(position, 0, 1)
 
 	def select(self, position):
@@ -1152,6 +1136,17 @@ class SelectionModel(ListModel, Gtk.SelectionModel):
 		if old_selected is not None:
 			self.selection_changed(old_selected, 1)
 
+	# Gio.ListModel methods
+	def do_get_item(self, position):
+		try:
+			return self._data[position]
+		except IndexError:
+			return None
+
+	def do_get_item_type(self): return self._item_type
+	def do_get_n_items(self): return len(self._data)
+
+	# Gtk.SelectionModel methods
 	def do_select_item(self, position, unselect_rest): return False
 	def do_select_all(self): return False
 	def do_select_range(self, position, n_items, unselect_rest): return False
@@ -1160,9 +1155,7 @@ class SelectionModel(ListModel, Gtk.SelectionModel):
 	def do_unselect_item(self, position): return False
 	def do_unselect_range(self, position, n_items): return False
 	def do_get_selection_in_range(self, position, n_items): return False
-
-	def do_is_selected(self, position):
-		return position == self._selected
+	def do_is_selected(self, position): return position == self._selected
 
 class SongMenu(Gtk.PopoverMenu):
 	def __init__(self, client, show_album=False):
