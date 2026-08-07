9de93ed608796da50bfda8be4b7c99921a792835
Martin Wagner <martin.wagner.dev@gmail.com>
2026-08-07 21:36:14 +0200

    Move `SongListRow`

--- src/plattenalbum.py
+++ src/plattenalbum.py
@@ -1084,36 +1084,6 @@ class HeadingBox(Gtk.Box):
 		self.append(Gtk.Label(label=heading, xalign=0, css_classes=["heading"]))
 		self.append(widget)
 
-class SongListRow(Gtk.Box):
-	position=GObject.Property(type=int, default=-1)
-	def __init__(self, show_track=True, **kwargs):
-		# can_target=False is needed to use Gtk.Widget.pick() in Gtk.ListView
-		super().__init__(can_target=False, **kwargs)
-
-		# labels
-		self._title=Gtk.Label(xalign=0, single_line_mode=True, ellipsize=Pango.EllipsizeMode.END)
-		self._subtitle=Gtk.Label(xalign=0, single_line_mode=True, ellipsize=Pango.EllipsizeMode.END, css_classes=["dimmed", "caption"])
-		self._length=Gtk.Label(xalign=1, single_line_mode=True, css_classes=["numeric", "dimmed"])
-
-		# packing
-		box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, valign=Gtk.Align.CENTER, hexpand=True)
-		box.append(self._title)
-		box.append(self._subtitle)
-		self.append(box)
-		self.append(self._length)
-
-	def set_song(self, song):
-		subtitle=str(song["artist"])
-		self._title.set_text(song["title"][0])
-		self._subtitle.set_visible(bool(subtitle))
-		self._subtitle.set_text(subtitle)
-		self._length.set_text(str(song["duration"]))
-
-	def unset_song(self):
-		self._title.set_text("")
-		self._subtitle.set_text("")
-		self._length.set_text("")
-
 class ListModel(GObject.Object, Gio.ListModel):
 	def __init__(self, item_type):
 		super().__init__()
@@ -1873,6 +1843,36 @@ class PlaylistMenu(Gtk.PopoverMenu):
 			self._show_album_action.set_enabled(self._client.can_show_album(self._song))
 			self._show_file_action.set_enabled(self._client.can_show_file(self._song))
 		self.popup()
+
+class SongListRow(Gtk.Box):
+	position=GObject.Property(type=int, default=-1)
+	def __init__(self, show_track=True, **kwargs):
+		# can_target=False is needed to use Gtk.Widget.pick() in Gtk.ListView
+		super().__init__(can_target=False, **kwargs)
+
+		# labels
+		self._title=Gtk.Label(xalign=0, single_line_mode=True, ellipsize=Pango.EllipsizeMode.END)
+		self._subtitle=Gtk.Label(xalign=0, single_line_mode=True, ellipsize=Pango.EllipsizeMode.END, css_classes=["dimmed", "caption"])
+		self._length=Gtk.Label(xalign=1, single_line_mode=True, css_classes=["numeric", "dimmed"])
+
+		# packing
+		box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, valign=Gtk.Align.CENTER, hexpand=True)
+		box.append(self._title)
+		box.append(self._subtitle)
+		self.append(box)
+		self.append(self._length)
+
+	def set_song(self, song):
+		subtitle=str(song["artist"])
+		self._title.set_text(song["title"][0])
+		self._subtitle.set_visible(bool(subtitle))
+		self._subtitle.set_text(subtitle)
+		self._length.set_text(str(song["duration"]))
+
+	def unset_song(self):
+		self._title.set_text("")
+		self._subtitle.set_text("")
+		self._length.set_text("")
 
 class PlaylistView(Gtk.ListView):
 	def __init__(self, client):
